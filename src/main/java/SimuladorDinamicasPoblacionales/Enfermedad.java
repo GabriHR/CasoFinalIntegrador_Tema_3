6c2f8b48560060c1b809008aff6ed51bb0691497
GabriHR <ghernrod@myuax.com>
2024-03-20 23:49:54 +0100

    clase enfermedad

---
+++ src/main/java/SimuladorDinamicasPoblacionales/Enfermedad.java
@@ -0,0 +1,15 @@
+package SimuladorDinamicasPoblacionales;
+
+import ModuladoEntidades.Animal;
+import java.util.List;
+import java.util.Random;
+
+public class Enfermedad extends EventosAleatorios {
+
+    @Override
+    public void ejecutar(List<Animal> animales) {
+        Random random = new Random();
+        Animal animal = animales.get(random.nextInt(animales.size()));
+        animal.salud -= Math.floor(Math.random() * 20);
+    }
+}
